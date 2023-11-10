171034585034fb009157530eed990fadf51c2848
wajdi2001 <wajdibbm@gmail.com>
2023-11-10 21:01:53 +0100

    commit finale

--- src/main/java/ACT5_1/metier/Compte.java
+++ src/main/java/ACT5_1/metier/Compte.java
@@ -1,52 +1,57 @@
 package ACT5_1.metier;
 
-
-import jakarta.jws.WebService;
 import jakarta.xml.bind.annotation.XmlAccessType;
 import jakarta.xml.bind.annotation.XmlAccessorType;
 import jakarta.xml.bind.annotation.XmlRootElement;
 import jakarta.xml.bind.annotation.XmlTransient;
-
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
+
+// Classe représentant un compte
+//@WebService
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Compte {
-    private int code =0;
-    private double solde=0.0;
+    private int code = 0;
+    private double solde = 0.0;
     @XmlTransient
     private Date date = new Date();
-    public Compte(int code, double solde, Date dateCreation) {
 
+    // Constructeur avec paramètres pour initialiser les attributs
+    public Compte(int code, double solde, Date dateCreation) {
         this.code = code;
         this.solde = solde;
         this.date = dateCreation;
     }
-    public Compte (){
+
+    // Constructeur par défaut
+    public Compte() {
 
     }
 
+    // Getter et Setter pour l'attribut 'code'
     public int getCode() {
         return code;
     }
+
     public void setCode(int code) {
         this.code = code;
     }
+
+    // Getter et Setter pour l'attribut 'solde'
     public double getSolde() {
         return solde;
     }
+
     public void setSolde(double solde) {
         this.solde = solde;
     }
+
+    // Getter et Setter pour l'attribut 'date'
     public Date getDateCreation() {
         return date;
     }
+
     public void setDateCreation(Date dateCreation) {
         this.date = dateCreation;
     }
-
-
-
 }
-
--- src/main/java/ACT5_1/service/BanqueService.java
+++ src/main/java/ACT5_1/service/BanqueService.java
@@ -1,39 +1,36 @@
 package ACT5_1.service;
 
-
 import ACT5_1.metier.Compte;
 import jakarta.jws.WebMethod;
 import jakarta.jws.WebParam;
 import jakarta.jws.WebService;
 
-
+import java.util.ArrayList;
 import java.util.Date;
 
-import java.util.ArrayList;
-
+// Service web JAX-WS représentant des opérations bancaires
 @WebService
-public class BanqueService
-{
-    public BanqueService(){}
-    public  ArrayList<Compte> comptes= new ArrayList<Compte>();
+public class BanqueService {
+
+    // Liste des comptes enregistrés
+    public ArrayList<Compte> comptes = new ArrayList<>();
+
+    // Constructeur par défaut
+    public BanqueService() {
+    }
+
+    // Méthode pour récupérer la liste des comptes
     @WebMethod
-    public ArrayList<Compte> getComptes()
-    {
-        comptes.add(new Compte(1,0.0,new Date()));
-        comptes.add(new Compte(2,0.0,new Date()));
-        comptes.add(new Compte(3,0.0,new Date()));
+    public ArrayList<Compte> getComptes() {
+        comptes.add(new Compte(1, 0.0, new Date()));
+        comptes.add(new Compte(2, 0.0, new Date()));
+        comptes.add(new Compte(3, 0.0, new Date()));
         return comptes;
-
     }
+
+    // Méthode de conversion de montant
     @WebMethod
-    public double conversion(@WebParam double montant)
-    {
-        return montant*3.3;
+    public double conversion(@WebParam double montant) {
+        return montant * 3.3;
     }
-
-
-
-
-
 }
-
